7dfef6966fc268f6e93a0b43162ff40e2f14576c
kieuphat159 <kieuphat159@gmail.com>
2025-09-12 17:42:32 +0700

    update db config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: '' + process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: 'ecommerce_db',
+  host: process.env.DB_HOST,       
+  port: process.env.DB_PORT,       
+  user: process.env.DB_USER,       
+  password: process.env.DB_PASS,  
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: {
+    ca: process.env.DB_CA
+  }
 });
 
 module.exports = pool.promise();
